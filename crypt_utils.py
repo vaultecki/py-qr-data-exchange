699cd11e5995eda778de15e4991bc236ceb26e3c
ecki <ecki@ecki-netz.de>
2025-09-03 19:33:45 +0200

    added debug logging

--- crypt_utils.py
+++ crypt_utils.py
@@ -9,11 +9,13 @@ logger = logging.getLogger(__name__)
 class CryptoUtils:
     @staticmethod
     def generate_salt() -> bytes:
+        logger.debug("generate salt")
         return nacl.utils.random(nacl.pwhash.argon2i.SALTBYTES)
 
     @staticmethod
     def derive_key(password: str, salt: bytes) -> bytes:
         """Leitet einen sicheren Schlüssel aus Passwort und Salt ab."""
+        logger.debug("derive key")
         password_bytes = password.encode("utf-8")
         kdf = nacl.pwhash.argon2i.kdf
         return kdf(nacl.secret.SecretBox.KEY_SIZE, password_bytes, salt)
@@ -21,12 +23,14 @@ class CryptoUtils:
     @staticmethod
     def encrypt(data: bytes, key: bytes) -> bytes:
         """Verschlüsselt Daten mit einem gegebenen Schlüssel."""
+        logger.debug("encrypt data")
         box = nacl.secret.SecretBox(key)
         return box.encrypt(data)
 
     @staticmethod
     def decrypt(encrypted_data: bytes, key: bytes) -> bytes:
         """Entschlüsselt Daten mit einem gegebenen Schlüssel."""
+        logger.debug("decrypt data")
         box = nacl.secret.SecretBox(key)
         return box.decrypt(encrypted_data)
 
